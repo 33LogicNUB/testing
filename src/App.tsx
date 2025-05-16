34be5caec4e3e576fab96fe9a3271b92b024a227
Shuvam Singh <143958008+33LogicNUB@users.noreply.github.com>
2025-05-16 14:37:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes';
-
+import TodoFormPage from './components/TodoFormPage';
 
 
 
@@ -9,6 +9,7 @@ function App() {
     <Router>
       <div className="max-w-xl mx-auto py-8">
         <h1 className="text-2xl font-bold mb-4 text-center">Todo App</h1>
+        <TodoFormPage />
         <AppRoutes />
         
       </div>
